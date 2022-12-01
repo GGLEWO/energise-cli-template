c5992af68fb8064470671f719c45388690a82da2
guanyaoming <guanyaoming@linklogis.com>
2022-12-01 17:27:44 +0800

    feat: 增加路由 json 文件

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,25 +1,21 @@
 export const routes = [
   {
     path: "/login",
-    component: () => require("../views/login/index"),
     hidden: true,
   },
 
   {
     path: "/404",
-    component: () => require("../views/404"),
     hidden: true,
   },
 
   {
     path: "/",
-    component: require("../layout/index.vue"),
     redirect: "/dashboard",
     children: [
       {
         path: "dashboard",
         name: "Dashboard",
-        component: () => require("../views/dashboard/index"),
         meta: { title: "Dashboard", icon: "dashboard" },
       },
     ],
@@ -27,24 +23,20 @@ export const routes = [
 
   {
     path: "/table",
-    component: require("../layout/index.vue"),
     children: [
       {
         path: "index",
         name: "table",
-        component: () => require("../views/table/index"),
         meta: { title: "table", icon: "table" },
       },
     ],
   },
   {
     path: "/tree",
-    component: require("../layout/index.vue"),
     children: [
       {
         path: "index",
         name: "tree",
-        component: () => require("../views/tree/index"),
         meta: { title: "tree", icon: "tree" },
       },
     ],
@@ -52,7 +44,6 @@ export const routes = [
 
   {
     path: "/form",
-    component: require("../layout/index.vue"),
     children: [
       {
         path: "index",
@@ -65,7 +56,6 @@ export const routes = [
 
   {
     path: "/nested",
-    component: require("../layout/index.vue"),
     redirect: "/nested/menu1",
     name: "Nested",
     meta: {
@@ -74,34 +64,27 @@ export const routes = [
     },
     children: [
       {
-        path: "menu1",
-        component: () => require("../views/nested/menu1/index"), // Parent router-view
+        path: "menu1",/ Parent router-view
         name: "Menu1",
         meta: { title: "Menu1" },
         children: [
           {
             path: "menu1-1",
-            component: () => require("../views/nested/menu1/menu1-1"),
             name: "Menu1-1",
             meta: { title: "Menu1-1" },
           },
           {
             path: "menu1-2",
-            component: () => require("../views/nested/menu1/menu1-2"),
             name: "Menu1-2",
             meta: { title: "Menu1-2" },
             children: [
               {
                 path: "menu1-2-1",
-                component: () =>
-                  require("../views/nested/menu1/menu1-2/menu1-2-1"),
                 name: "Menu1-2-1",
                 meta: { title: "Menu1-2-1" },
               },
               {
                 path: "menu1-2-2",
-                component: () =>
-                  require("../views/nested/menu1/menu1-2/menu1-2-2"),
                 name: "Menu1-2-2",
                 meta: { title: "Menu1-2-2" },
               },
@@ -109,7 +92,6 @@ export const routes = [
           },
           {
             path: "menu1-3",
-            component: () => require("../views/nested/menu1/menu1-3"),
             name: "Menu1-3",
             meta: { title: "Menu1-3" },
           },
@@ -117,7 +99,6 @@ export const routes = [
       },
       {
         path: "menu2",
-        component: () => require("../views/nested/menu2/index"),
         name: "Menu2",
         meta: { title: "menu2" },
       },
@@ -126,7 +107,6 @@ export const routes = [
 
   {
     path: "external-link",
-    component: require("../layout/index.vue"),
     children: [
       {
         path: "https://panjiachen.github.io/vue-element-admin-site/#/",
