47ef3bbab80ab4b09ad238d1fb7f921727284f51
guanyaoming <guanyaoming@linklogis.com>
2022-12-01 17:18:12 +0800

    fix: 更改组件的引入方式

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,26 +1,25 @@
-import Layout from "@/layout";
 export const routes = [
   {
     path: "/login",
-    component: () => import("@/views/login/index"),
+    component: () => require("@/views/login/index"),
     hidden: true,
   },
 
   {
     path: "/404",
-    component: () => import("@/views/404"),
+    component: () => require("@/views/404"),
     hidden: true,
   },
 
   {
     path: "/",
-    component: Layout,
+    component: require("@/layout"),
     redirect: "/dashboard",
     children: [
       {
         path: "dashboard",
         name: "Dashboard",
-        component: () => import("@/views/dashboard/index"),
+        component: () => require("@/views/dashboard/index"),
         meta: { title: "Dashboard", icon: "dashboard" },
       },
     ],
@@ -28,24 +27,24 @@ export const routes = [
 
   {
     path: "/table",
-    component: Layout,
+    component: require("@/layout"),
     children: [
       {
         path: "index",
         name: "table",
-        component: () => import("@/views/table/index"),
+        component: () => require("@/views/table/index"),
         meta: { title: "table", icon: "table" },
       },
     ],
   },
   {
     path: "/tree",
-    component: Layout,
+    component: require("@/layout"),
     children: [
       {
         path: "index",
         name: "tree",
-        component: () => import("@/views/tree/index"),
+        component: () => require("@/views/tree/index"),
         meta: { title: "tree", icon: "tree" },
       },
     ],
@@ -53,12 +52,12 @@ export const routes = [
 
   {
     path: "/form",
-    component: Layout,
+    component: require("@/layout"),
     children: [
       {
         path: "index",
         name: "Form",
-        component: () => import("@/views/form/index"),
+        component: () => require("@/views/form/index"),
         meta: { title: "Form", icon: "form" },
       },
     ],
@@ -66,7 +65,7 @@ export const routes = [
 
   {
     path: "/nested",
-    component: Layout,
+    component: require("@/layout"),
     redirect: "/nested/menu1",
     name: "Nested",
     meta: {
@@ -76,33 +75,33 @@ export const routes = [
     children: [
       {
         path: "menu1",
-        component: () => import("@/views/nested/menu1/index"), // Parent router-view
+        component: () => require("@/views/nested/menu1/index"), // Parent router-view
         name: "Menu1",
         meta: { title: "Menu1" },
         children: [
           {
             path: "menu1-1",
-            component: () => import("@/views/nested/menu1/menu1-1"),
+            component: () => require("@/views/nested/menu1/menu1-1"),
             name: "Menu1-1",
             meta: { title: "Menu1-1" },
           },
           {
             path: "menu1-2",
-            component: () => import("@/views/nested/menu1/menu1-2"),
+            component: () => require("@/views/nested/menu1/menu1-2"),
             name: "Menu1-2",
             meta: { title: "Menu1-2" },
             children: [
               {
                 path: "menu1-2-1",
                 component: () =>
-                  import("@/views/nested/menu1/menu1-2/menu1-2-1"),
+                  require("@/views/nested/menu1/menu1-2/menu1-2-1"),
                 name: "Menu1-2-1",
                 meta: { title: "Menu1-2-1" },
               },
               {
                 path: "menu1-2-2",
                 component: () =>
-                  import("@/views/nested/menu1/menu1-2/menu1-2-2"),
+                  require("@/views/nested/menu1/menu1-2/menu1-2-2"),
                 name: "Menu1-2-2",
                 meta: { title: "Menu1-2-2" },
               },
@@ -110,7 +109,7 @@ export const routes = [
           },
           {
             path: "menu1-3",
-            component: () => import("@/views/nested/menu1/menu1-3"),
+            component: () => require("@/views/nested/menu1/menu1-3"),
             name: "Menu1-3",
             meta: { title: "Menu1-3" },
           },
@@ -118,7 +117,7 @@ export const routes = [
       },
       {
         path: "menu2",
-        component: () => import("@/views/nested/menu2/index"),
+        component: () => require("@/views/nested/menu2/index"),
         name: "Menu2",
         meta: { title: "menu2" },
       },
@@ -127,7 +126,7 @@ export const routes = [
 
   {
     path: "external-link",
-    component: Layout,
+    component: require("@/layout"),
     children: [
       {
         path: "https://panjiachen.github.io/vue-element-admin-site/#/",
