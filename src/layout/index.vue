6497af6bc0100f6f32ac71d1d57707c9c53ee168
guanyaoming <guanyaoming@linklogis.com>
2023-05-05 10:36:19 +0800

    fix: 去除敏感信息

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: guanyaoming 
  * @Date: 2022-11-30 14:13:40
- * @LastEditors: guanyaoming guanyaoming@linklogis.com
- * @LastEditTime: 2023-05-05 10:18:55
+ * @LastEditors: guanyaoming
+ * @LastEditTime: 2023-05-05 10:35:34
  * @FilePath: \energise-cli-template\src\layout\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
